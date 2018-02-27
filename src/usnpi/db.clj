b66d324414bfad32428a4be226546d4b33f6e0fd
Ivan Grishaev <ivan@grishaev.me>
2018-02-27 13:15:46 +0300

    3 db module added

---
+++ src/usnpi/db.clj
@@ -0,0 +1,34 @@
+(ns usnpi.db
+  (:require [clojure.java.jdbc :as jdbc]
+            [environ.core :as env]))
+
+(def ^:private
+  ;; db-url "jdbc:postgresql://localhost:5678/usnpi?stringtype=unspecified&user=postgres&password=verysecret"
+  db-url "jdbc:postgresql://localhost:5432/usnpi?stringtype=unspecified&user=usnpi&password=usnpi"
+  )
+
+(def ^:private ^:dynamic
+  *db* {:dbtype "postgresql"
+        :connection-uri (or (env/env :database-url) db-url)})
+
+(defn query [& args]
+  (apply jdbc/query *db* args))
+
+(defn update! [& args]
+  (apply jdbc/update! *db* args))
+
+(defn delete! [& args]
+  (apply jdbc/delete! *db* args))
+
+(defn execute! [& args]
+  (apply jdbc/execute! *db* args))
+
+(defmacro with-trx [& body]
+  `(jdbc/with-db-transaction [tx# *db*]
+     (binding [*db* tx#]
+       ~@body)))
+
+(defmacro with-trx-test [& body]
+  `(with-trx
+     (jdbc/db-set-rollback-only! *db*)
+     ~@body))
