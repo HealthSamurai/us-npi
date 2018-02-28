e0ca9c1da4a8053ea81109758fb8fa1f97e34509
Ivan Grishaev <ivan@grishaev.me>
2018-02-28 13:54:57 +0300

    3 deactivate task works

--- src/usnpi/updater.clj
+++ src/usnpi/updater.clj
@@ -1,6 +1,8 @@
 (ns usnpi.updater
   (:require [usnpi.db :as db]
+            [usnpi.util :refer [raise!]]
             [clojure.tools.logging :as log]
+            [clojure.string :as str]
             [dk.ative.docjure.spreadsheet :as xls]
             [clj-http.client :as client]
             [hickory.core :as hickory]
@@ -9,13 +11,16 @@
            java.util.zip.ZipInputStream))
 
 (def ^:private
+  db-chunk 100)
+
+(def ^:private
   path-base "http://download.cms.gov/nppes/")
 
 (def ^:private
   path-dl "NPI_Files.html")
 
-(defn- parse-page []
-  (-> (str path-base path-dl)
+(defn- parse-page [url]
+  (-> url
       client/get
       :body
       hickory.core/parse
@@ -38,43 +43,79 @@
 (defn- full-url [href]
   (str path-base href))
 
-(defn- get-deactive-url [page]
-  (when-let [node (first (s/select deactive-selector page))]
+(defn- get-deactive-url [page-tree]
+  (when-let [node (last (s/select deactive-selector page-tree))]
     (let [href (-> node :attrs :href)]
       (full-url href))))
 
-;; "http://download.cms.gov/nppes/NPPES_Deactivated_NPI_Report_021318.zip"
+(defn- ^ZipInputStream get-stream
+  [url]
+  (let [resp (client/get url {:as :stream})]
+    (ZipInputStream. (:body resp))))
 
-(defn- get-deactive-stream [url]
-  (let [resp (client/get url {:as :stream})
-        stream (ZipInputStream. (:body resp))]
-    (doto stream .getNextEntry)))
+(defn- matches-ext [filename ext]
+  (str/ends-with? (str/lower-case filename) (str/lower-case ext)))
+
+(defn- seek-stream
+  [^ZipInputStream stream ^String ext]
+  (loop []
+    (if-let [entry (.getNextEntry stream)]
+      (let [filename (.getName entry)]
+        (if (matches-ext filename ext)
+          stream
+          (recur)))
+      (raise! "Cannot find a %s file in a stream" ext))))
+
+(defn- seek-deactive-stream
+  [^ZipInputStream stream]
+  (.getNextEntry stream))
 
 (defn- read-deactive-npis [source]
   (let [wb (xls/load-workbook source)
         sheet (first (xls/sheet-seq wb))
         cell (xls/select-columns {:A :npi} sheet)
         header 2]
-    (take 10 (map :npi (drop header cell)))))
+    (mapv :npi (drop header cell))))
 
 (defn- by-chunks [n seq]
   (partition n n [] seq))
 
 (defn- mark-npi-deleted [npis-all]
   (db/with-tx
-    (doseq [npis (by-chunks 100 npis-all)]
+    (doseq [npis (by-chunks db-chunk npis-all)]
       (db/execute!
        (db/to-sql
         {:update :practitioner
          :set {:deleted true}
-         :where [:in :npi npis]})))))
+         :where [:in :id npis]})))))
 
 (defn task-deactivate []
-  (let [page (parse-page)
-        url (get-deactive-url page)
-        stream (get-deactive-stream url)
-        npis (read-deactive-npis stream)]
+  (let [url-page (str path-base path-dl)
+
+        _ (log/infof "Parsing %s page..." url-page)
+        page-tree (parse-page url-page)
+
+        url-zip (get-deactive-url page-tree)
+        _ (log/infof "Deactivation URL is %s" url-zip)
+
+        _ (when-not url-zip
+            (raise! "Deactivation URL doesn't present on the page %s" url-page))
+
+        stream (get-stream url-zip)
+
+        _ (log/infof "Seeking stream for an Excel file...")
+        stream (seek-stream stream ".xlsx")
+
+        _ (log/infof "Reading NPIs from a file...")
+        npis (read-deactive-npis stream)
+        _ (log/infof "Found %s NPIs to deactive" (count npis))
+
+        ]
+
+    (log/infof "Marking NPIs as deleted with a step of %s" db-chunk)
     (mark-npi-deleted npis)
+    (log/infof "Done.")
+
     nil))
 
 (defn task-dissemination []
