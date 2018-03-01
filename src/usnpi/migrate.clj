cd4fd8b3275d5ad50421855b6afadc30681aa530
Ivan Grishaev <ivan@grishaev.me>
2018-03-01 13:32:41 +0300

    3 migrations module refactored

--- src/usnpi/migrate.clj
+++ src/usnpi/migrate.clj
@@ -1,33 +1,45 @@
 (ns usnpi.migrate
+  "Small module to deal with migrations. These are .sql files
+  put into resources/migrations folder. Prepend files' names with
+  a prefix for proper ordering."
   (:require [usnpi.db :as db]
+            [clojure.string :as s]
+            [clojure.java.io :as io]
             [clojure.tools.logging :as log]))
 
-(def ^:private
-  migrations
-  [
-   "
-create table if not exists tasks (
-    id          serial primary key,
-    handler     text not null,
-    interval    integer not null default 0,
-    last_run_at timestamp with time zone null,
-    next_run_at timestamp with time zone not null,
-    success     boolean not null default false,
-    message     text not null default ''
-);"
+(defn- file-name
+  [^java.io.File file]
+  (.getName file))
 
-   "
-alter table practitioner
-    add column if not exists
-    deleted boolean not null default false;
-"
-   ])
+(defn- is-sql?
+  [^java.io.File file]
+  (-> file file-name (s/ends-with? ".sql")))
+
+(defn- to-migration
+  [^java.io.File file]
+  {:name (file-name file)
+   :body (slurp file)})
+
+(defn- read-migrations
+  [^String res]
+  (if-let [mig-dir (-> res io/resource io/file)]
+    (->> mig-dir
+         file-seq
+         (filter is-sql?)
+         (sort-by file-name)
+         (map to-migration))
+    (throw (Exception.
+            (format "Cannot read migrations from: %s" res)))))
+
+;;
+;; public
+;;
 
 (defn migrate []
   (log/info "Running migrations...")
 
-  (doseq [[i mig] (map-indexed vector migrations)]
-    (log/infof "Migration: %s" i)
-    (db/execute! mig))
+  (doseq [{:keys [name body]} (read-migrations "migrations")]
+    (log/infof "Migration: %s" name)
+    (db/execute! body))
 
   (log/info "Done."))
