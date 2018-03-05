7023f77e14b93f7ca046bf3860d7f8340f81a3ff
Ivan Grishaev <ivan@grishaev.me>
2018-03-05 15:00:41 +0300

    12 items column dropped

--- resources/migrations/20180305080038-add-update-files-table.up.sql
+++ resources/migrations/20180305080038-add-update-files-table.up.sql
@@ -4,7 +4,6 @@ create table if not exists npi_updates (
     id serial primary key,
     type text not null,
     date timestamp with time zone not null default (current_timestamp at time zone 'UTC'),
-    items integer not null default 0,
     url text not null
 );
 
