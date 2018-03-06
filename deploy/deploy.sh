1296688e9e9e61eeb7f5941be674d68bbb21a84c
Ivan Grishaev <ivan@grishaev.me>
2018-03-06 12:35:52 +0300

    sed command fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,17 +6,17 @@ BRANCH=$1 # master
 IMAGE=$2
 
 cat deploy.tpl.yaml \
-    | sed "s|<BRANCH>|$BRANCH|g" \
-    | sed "s|<IMAGE>|$IMAGE|g" \
-    | sed "s|<GIT_COMMIT>|$GIT_COMMIT|g" \
-    | sed "s|<DB_HOST>|$DB_HOST|g" \
-    | sed "s|<DB_PORT>|$DB_PORT|g" \
-    | sed "s|<DB_DATABASE>|$DB_DATABASE|g" \
-    | sed "s|<DB_USER>|$DB_USER|g" \
-    | sed "s|<DB_PASSWORD>|$DB_PASSWORD|g" \
-    | sed "s|<PORT>|$PORT|g" \
-    | sed "s|<FHIRTERM_BASE>|$FHIRTERM_BASE|g" \
-    | sed "s|<BEAT_TIMEOUT>|$BEAT_TIMEOUT|g" \
+    | sed -e "s|<BRANCH>|$BRANCH|g" \
+    | sed -e "s|<IMAGE>|$IMAGE|g" \
+    | sed -e "s|<GIT_COMMIT>|$GIT_COMMIT|g" \
+    | sed -e "s|<DB_HOST>|$DB_HOST|g" \
+    | sed -e "s|<DB_PORT>|$DB_PORT|g" \
+    | sed -e "s|<DB_DATABASE>|$DB_DATABASE|g" \
+    | sed -e "s|<DB_USER>|$DB_USER|g" \
+    | sed -e "s|<DB_PASSWORD>|$DB_PASSWORD|g" \
+    | sed -e "s|<PORT>|$PORT|g" \
+    | sed -e "s|<FHIRTERM_BASE>|$FHIRTERM_BASE|g" \
+    | sed -e "s|<BEAT_TIMEOUT>|$BEAT_TIMEOUT|g" \
     > deploy.yaml
 
 cat deploy.yaml
