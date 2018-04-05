5f983262356bdead60216d17d3f61179329dfd12
Ivan Grishaev <ivan@grishaev.me>
2018-04-05 12:40:38 +0300

    http response for index

--- src/usnpi/swagger.clj
+++ src/usnpi/swagger.clj
@@ -1,10 +1,9 @@
 (ns usnpi.swagger
   (:require [clojure.java.io :as io]
-            [usnpi.http :refer [json-resp]]))
+            [usnpi.http :refer [json-resp http-resp]]))
 
 (defn api-index [request]
-  {:status 200
-   :body (-> "swagger/index.html" io/resource slurp)})
+  (http-resp (-> "swagger/index.html" io/resource slurp)))
 
 (defn api-schema [request]
   (json-resp (-> "swagger/swagger.edn" io/resource slurp read-string)))
